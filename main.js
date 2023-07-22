3fc4a8e88e216207ed271f9f5774a2f4878694dc
fath-nasrudin <fath.nasrudin.i@gmail.com>
2023-07-22 21:49:26 +0700

    feat: Connect UI and logic

--- main.js
+++ main.js
@@ -37,3 +37,58 @@ const helper = {
     return string.split(/([-+*/])/g);
   },
 };
+
+const webUI = {
+  displayValue: 0,
+  getDisplayValue() {
+    return this.displayValue;
+  },
+  setDisplayValue(value) {
+    this.displayValue = value;
+  },
+  addLastDisplayValue(newValue) {
+    let displayValue = this.getDisplayValue();
+    if (displayValue === 0) {
+      this.setDisplayValue(newValue);
+    } else {
+      this.setDisplayValue(displayValue + newValue);
+    }
+  },
+  uiUpdateDisplayValue() {
+    const calculatorDisplay = document.querySelector('#calculatorDisplay');
+    calculatorDisplay.textContent = this.getDisplayValue();
+  },
+  uiSetAndUpdateDisplayValue(value) {
+    this.setDisplayValue(value);
+    this.uiUpdateDisplayValue();
+  },
+  keypadHandler(e) {
+    this.addLastDisplayValue(e.currentTarget.value);
+    this.uiUpdateDisplayValue();
+  },
+  equalButtonHandler(e) {
+    const result = calculator.calculate(this.getDisplayValue());
+    this.uiSetAndUpdateDisplayValue(result);
+  },
+  clearHandler(e) {
+    this.uiSetAndUpdateDisplayValue(0);
+  },
+  populateKeypadListeners(e) {
+    const keyNumbers = document.querySelectorAll(
+      '.key-num, .add-operator, .subtract-operator, .multiply-operator, .divide-operator'
+    );
+    Array.from(keyNumbers).forEach((keyNum) => {
+      keyNum.addEventListener('click', this.keypadHandler.bind(webUI));
+    });
+
+    const equalButton = document.querySelector('.equal-operator');
+    equalButton.addEventListener('click', this.equalButtonHandler.bind(webUI));
+
+    const clearButton = document.querySelector('.clear-operator');
+    console.log(clearButton);
+    clearButton.addEventListener('click', this.clearHandler.bind(webUI));
+  },
+};
+
+webUI.uiUpdateDisplayValue();
+webUI.populateKeypadListeners();
